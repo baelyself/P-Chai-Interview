604c273e9f3d33e69f1e954d6c59cba93525ad73
baelyself <164568314+baelyself@users.noreply.github.com>
2025-10-06 15:39:56 +0700

    Add initial website files for P’Chai interview

---
+++ script.js
@@ -0,0 +1,6 @@
+// สามารถเพิ่มฟังก์ชัน custom audio ได้ในนี้
+const audio = document.getElementById("audio");
+// ตัวอย่าง custom play/pause
+// document.getElementById("playBtn").addEventListener("click", () => {
+//   audio.play();
+// });
